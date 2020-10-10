2d7a90aed58168bc1ef7bcd0552e97f149e337ca
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-10 14:02:52 +0530

    [Arijit]Added : Append Nodes To LinkedList

--- src/test/java/com/cg/NodeTest.java
+++ src/test/java/com/cg/NodeTest.java
@@ -33,4 +33,18 @@ public class NodeTest {
 				&& linkedList.tail.equals(firstNode);
 		assertTrue(result);
 	}
+    @Test
+ 	public void given3NumbersWhenAddedToLinkedListShouldBeAddedToLast() {
+ 		Node<Integer> firstNode = new Node<>(70);
+ 		Node<Integer> secondNode = new Node<>(30);
+ 		Node<Integer> thirdNode = new Node<>(56);
+ 		LinkedList<Integer> linkedList = new LinkedList<>();
+ 		linkedList.add(firstNode);
+ 		linkedList.append(secondNode);
+ 		linkedList.append(thirdNode);
+ 		linkedList.print();
+ 		boolean result = linkedList.head.equals(firstNode) && linkedList.head.getNext().equals(secondNode)
+ 				&& linkedList.tail.equals(thirdNode);
+ 		assertTrue(result);
+ 	}
 }
