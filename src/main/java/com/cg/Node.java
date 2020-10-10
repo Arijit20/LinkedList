0876354f10868df145c90b1cf17314162ac57a66
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-10 13:06:50 +0530

    [Arijit]Added : Starting LinkedList

---
+++ src/main/java/com/cg/Node.java
@@ -0,0 +1,7 @@
+package com.cg;
+
+public class Node{
+   public static void main(String[] args) {
+	   System.out.println("Starting LinkedList");
+   }
+}
---
+++ src/test/java/com/cg/NodeTest.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.junit.Test;
+
+import com.cg.Node;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+public class NodeTest {
+ 
+}
